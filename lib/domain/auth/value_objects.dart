f56694b9409941cb0bf91ce1940ba1a3d3d05037
Piotr <piotrekert90@gmail.com>
2020-03-22 19:06:39 +0100

    Add simple password validation. Remove disused import.

--- lib/domain/auth/value_objects.dart
+++ lib/domain/auth/value_objects.dart
@@ -16,3 +16,17 @@ class EmailAddress extends ValueObject<String>{
 
   const EmailAddress._(this.value);
 }
+
+class Password extends ValueObject<String> {
+  @override
+  final Either<ValueFailure<String>, String> value;
+
+  factory Password(String input) {
+    assert(input != null);
+    return Password._(
+      validatePassword(input),
+    );
+  }
+
+  const Password._(this.value);
+}
--- lib/domain/core/value_validators.dart
+++ lib/domain/core/value_validators.dart
@@ -2,12 +2,21 @@ import 'package:dartz/dartz.dart';
 import 'package:notes/domain/core/failures.dart';
 
 Either<ValueFailure<String>, String> validateEmailAddress(String input) {
-  //FIXME: change this regexp pattern with more robust
+  //TODO: change this regexp pattern with more robust
   const emailRegex =
       r"""^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+""";
   if (RegExp(emailRegex).hasMatch(input)) {
     return right(input);
   } else {
     return left(ValueFailure.invalidEmail(failedValue: input));
+  }
+}
+
+Either<ValueFailure<String>, String> validatePassword(String input) {
+  //TODO: Add advanced password checks (uppercase/lowercase, at least 1 number, ...)
+  if (input.length >= 6) {
+    return right(input);
+  } else {
+    return left(ValueFailure.shortPassword(failedValue: input));
   }
 }
