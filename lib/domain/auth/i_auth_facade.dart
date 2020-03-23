63c51582207db28527b80efd824033f1cc68e5ef
Piotr <piotrekert90@gmail.com>
2020-03-23 19:58:33 +0100

    Update IAuthFacade using Either union and AuthFailure.

--- lib/domain/auth/i_auth_facade.dart
+++ lib/domain/auth/i_auth_facade.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/foundation.dart';
+import 'package:dartz/dartz.dart';
 import 'package:notes/domain/auth/value_objects.dart';
+import 'package:notes/domain/auth/auth_failure.dart';
 
 //TODO: Create FirebaseAuth and GoogleSignIn implementation
 abstract class IAuthFacade {
-  Future<void> registerWithEmailAndPassword({
+  Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword({
     @required EmailAddress emailAddress,
     @required Password password,
   });
-  
-  Future<void> signInWithEmailAndPassword({
+  Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword({
     @required EmailAddress emailAddress,
     @required Password password,
   });
-  
-  Future<void> signInWithGoogle();
+  Future<Either<AuthFailure, Unit>> signInWithGoogle();
 }
