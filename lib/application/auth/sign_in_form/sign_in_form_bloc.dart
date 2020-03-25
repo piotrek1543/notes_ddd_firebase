931b0f8d14d1871fa64e3b8513553e0761cd2087
Piotr <piotrekert90@gmail.com>
2020-03-25 20:45:28 +0100

    Update sign_in_form.bloc.dart.

--- lib/application/auth/sign_in_form/sign_in_form_bloc.dart
+++ lib/application/auth/sign_in_form/sign_in_form_bloc.dart
@@ -2,9 +2,12 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'sign_in_form_event.dart';
 part 'sign_in_form_state.dart';
+
+part 'sign_in_form_bloc.freezed.dart';
 
 class SignInFormBloc extends Bloc<SignInFormEvent, SignInFormState> {
   @override
