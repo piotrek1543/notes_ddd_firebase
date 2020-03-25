89010299b6d57ec77261ec7298a0f3c67d6fa86a
Piotr <piotrekert90@gmail.com>
2020-03-25 21:06:04 +0100

    Update sign_in_form_state.dart.

--- lib/application/auth/sign_in_form/sign_in_form_state.dart
+++ lib/application/auth/sign_in_form/sign_in_form_state.dart
@@ -1,6 +1,20 @@
 part of 'sign_in_form_bloc.dart';
 
-@immutable
-abstract class SignInFormState {}
+@freezed
+abstract class SignInFormState with _$SignInFormState {
+  const factory SignInFormState({
+    @required EmailAddress emailAddress,
+    @required Password password,
+    @required bool showErrorMessages,
+    @required bool isSubmitting,
+    @required Option<Either<AuthFailure, Unit>> authFailureOrSuccessOption,
+  }) = _SignInFormState;
 
-class SignInFormInitial extends SignInFormState {}
+  factory SignInFormState.initial() => SignInFormState(
+        emailAddress: EmailAddress(''),
+        password: Password(''),
+        showErrorMessages: false,
+        isSubmitting: false,
+        authFailureOrSuccessOption: none(),
+      );
+}
