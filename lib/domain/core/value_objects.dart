6e798c4a3f21510cc0da27bcb8d8b6aabb03623a
Piotr <piotrekert90@gmail.com>
2020-04-14 21:55:15 +0200

    Create errors.dart. Add `getOrCrash` generic method.

--- lib/domain/core/value_objects.dart
+++ lib/domain/core/value_objects.dart
@@ -4,10 +4,18 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:flutter/foundation.dart';
 import 'package:notes/domain/core/failures.dart';
 
+import 'errors.dart';
+
 @immutable
 abstract class ValueObject<T> {
   const ValueObject();
   Either<ValueFailure<T>, T> get value;
+
+   /// Throws [UnexpectedValueError] containing the [ValueFailure]
+  T getOrCrash() {
+    // id = identity - same as writing (right) => right
+    return value.fold((f) => throw UnexpectedValueError(f), id);
+  }
 
   bool isValid() => value.isRight(); 
 
