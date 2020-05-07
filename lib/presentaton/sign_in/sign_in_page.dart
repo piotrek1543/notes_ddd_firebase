a71849410bbe046d1e759019d68a30e33faf10c2
Piotr <piotrekert90@gmail.com>
2020-05-07 19:29:39 +0200

    Create SignInForm widget. Update SignInPage widget.

--- lib/presentaton/sign_in/sign_in_page.dart
+++ lib/presentaton/sign_in/sign_in_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes/application/auth/sign_in_form/sign_in_form_bloc.dart';
+import 'package:notes/injection.dart';
+import 'package:notes/presentaton/sign_in/sign_in_form.dart';
 
 class SignInPage extends StatelessWidget {
   @override
@@ -6,6 +10,10 @@ class SignInPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Sign In'),
+      ),
+      body: BlocProvider(
+        create: (context) => getIt<SignInFormBloc>(),
+        child: SignInForm(),
       ),
     );
   }
