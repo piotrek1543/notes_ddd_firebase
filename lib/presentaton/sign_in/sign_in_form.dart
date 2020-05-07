39bdd42ee651cdec0dc22cc4fa92c4855038072b
Piotr <piotrekert90@gmail.com>
2020-05-07 20:01:53 +0200

    Working on SignInForm - add logo and TextFormFields.

--- lib/presentaton/sign_in/sign_in_form.dart
+++ lib/presentaton/sign_in/sign_in_form.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes/application/auth/sign_in_form/sign_in_form_bloc.dart';
 
 class SignInForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return BlocConsumer<SignInFormBloc, SignInFormState>(
+      listener: (context, state) {},
+      builder: (context, state) {
+        return Form(
+          child: ListView(
+            children: <Widget>[
+              const Text(
+                '📝',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 130),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.email),
+                  labelText: 'Email',
+                ),
+                autocorrect: false,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.lock),
+                  labelText: 'Password',
+                ),
+                autocorrect: false,
+                obscureText: true,
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
