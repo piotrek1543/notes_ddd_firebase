53105081ab351cc3e1bd1fbea47a3bf3e24f654c
Piotr <piotrekert90@gmail.com>
2020-05-07 19:21:12 +0200

    Rename NotesApp to AppWidget. Move to presentation

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
 import 'package:notes/injection.dart';
-import 'package:notes/notes_app.dart';
+import 'package:notes/presentaton/core/app_widget.dart';
 
 void main() {
   configureInjection(Environment.prod);
-  runApp(NotesApp());
+  runApp(AppWidget());
 }
