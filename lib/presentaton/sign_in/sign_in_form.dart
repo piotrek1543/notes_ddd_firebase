a44157788b5b806d123d2657cac4e4f7ed0eb080
Piotr <piotrekert90@gmail.com>
2020-05-08 06:35:09 +0200

    Add error handling using flushbar to SignInForm.

--- lib/presentaton/sign_in/sign_in_form.dart
+++ lib/presentaton/sign_in/sign_in_form.dart
@@ -1,3 +1,4 @@
+import 'package:flushbar/flushbar_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes/application/auth/sign_in_form/sign_in_form_bloc.dart';
@@ -6,12 +7,32 @@ class SignInForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<SignInFormBloc, SignInFormState>(
-      listener: (context, state) {},
+      listener: (context, state) {
+        state.authFailureOrSuccessOption.fold(
+          () {},
+          (either) => either.fold(
+            (failure) {
+              FlushbarHelper.createError(
+                message: failure.map(
+                  cancelledByUser: (_) => 'Cancelled',
+                  serverError: (_) => 'Server error',
+                  emailAlreadyInUse: (_) => 'Email already in use',
+                  invalidEmailAndPasswordCombination: (_) =>
+                      'Invalid email and password combination',
+                ),
+              ).show(context);
+            },
+            (_) {
+              // TODO: Navigate
+            },
+          ),
+        );
+      },
       builder: (context, state) {
         return Form(
           autovalidate: state.showErrorMessages,
           child: ListView(
-            children: <Widget>[
+            children: [
               const Text(
                 '📝',
                 textAlign: TextAlign.center,
@@ -62,7 +83,7 @@ class SignInForm extends StatelessWidget {
               ),
               const SizedBox(height: 8),
               Row(
-                children: <Widget>[
+                children: [
                   Expanded(
                     child: FlatButton(
                       onPressed: () {
