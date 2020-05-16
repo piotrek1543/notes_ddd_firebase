0d3310fe5808eaf42a838eeb2d87fb3c14088636
Piotr <piotrekert90@gmail.com>
2020-05-16 08:27:18 +0200

    Update User entity and UniqueId value object.

--- lib/domain/core/value_objects.dart
+++ lib/domain/core/value_objects.dart
@@ -38,12 +38,14 @@ class UniqueId extends ValueObject<String> {
   final Either<ValueFailure<String>, String> value;
 
   factory UniqueId() {
-    return UniqueId._(right(Uuid().v1()));
+    return UniqueId._(
+      right(Uuid().v1()),
+    );
   }
 
-  factory UniqueId.fromUniqueString(String uniqueString) {
-    assert(uniqueString != null);
-    return UniqueId._(right(uniqueString));
+  factory UniqueId.fromUniqueString(String uniqueId) {
+    assert(uniqueId != null);
+    return UniqueId._(right(uniqueId));
   }
 
   const UniqueId._(this.value);
