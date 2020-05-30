3a4ab1df784746f402cf7ae9d0f1ebae82bd4aef
Piotr <piotrekert90@gmail.com>
2020-05-30 08:41:15 +0200

    Add implementaion of mapEventToState in AuthBloc.

--- lib/application/auth/auth_bloc.dart
+++ lib/application/auth/auth_bloc.dart
@@ -2,14 +2,21 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:injectable/injectable.dart';
 import 'package:meta/meta.dart';
+import 'package:notes/domain/auth/i_auth_facade.dart';
 
 part 'auth_event.dart';
 part 'auth_state.dart';
 
 part 'auth_bloc.freezed.dart';
 
+@injectable
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  final IAuthFacade _authFacade;
+
+  AuthBloc(this._authFacade);
+
   @override
   AuthState get initialState => AuthState.initial();
 
@@ -17,6 +24,18 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
   Stream<AuthState> mapEventToState(
     AuthEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+    yield* event.map(
+      authCheckRequested: (e) async* {
+        final userOption = await _authFacade.getSignedInUser();
+        yield userOption.fold(
+          () => const AuthState.unauthenticated(),
+          (_) => const AuthState.authenticated(),
+        );
+      },
+      signedOut: (e) async* {
+        await _authFacade.signOut();
+        yield const AuthState.unauthenticated();
+      },
+    );
   }
 }
