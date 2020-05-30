ba9c682d92167fa346adc903b4ecb05c252179d8
Piotr <piotrekert90@gmail.com>
2020-05-30 10:11:16 +0200

    Add MultiBlocProvider and ExtendedNavigator.

--- lib/presentaton/core/app_widget.dart
+++ lib/presentaton/core/app_widget.dart
@@ -1,19 +1,31 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:notes/presentaton/sign_in/sign_in_page.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes/application/auth/auth_bloc.dart';
+import 'package:notes/injection.dart';
+import 'package:notes/presentaton/routes/router.gr.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Notes',
-      home: SignInPage(),
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.light().copyWith(
-        primaryColor: Colors.green[800],
-        accentColor: Colors.blueAccent,
-        inputDecorationTheme: InputDecorationTheme(
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(8),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) =>
+              getIt<AuthBloc>()..add(const AuthEvent.authCheckRequested()),
+        )
+      ],
+      child: MaterialApp(
+        title: 'Notes',
+        builder: ExtendedNavigator(router: Router()),
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.light().copyWith(
+          primaryColor: Colors.green[800],
+          accentColor: Colors.blueAccent,
+          inputDecorationTheme: InputDecorationTheme(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
           ),
         ),
       ),
